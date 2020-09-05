393b1f0620ec7e79821b01518689036d0ffd41bc
Filip Björck <filip.bjorck@gmail.com>
2020-09-05 19:07:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Blinking LED for WeAct STM32F411 MiniF4 
+
+Blink onboard LED.
+
+# Prereqs
+ 
+rust target
+
+```sh
+$ rustup target install thumbv7em-none-eabihf
+```
+
+cargo-binutils
+
+```sh
+$ cargo install cargo-binutils
+$ rustup component add llvm-tools-preview
+```
+
+dfu-util
+
+```sh
+$ brew install dfu-util
+```
+
+# Building
+
+```sh
+$ cargo objcopy --release -- -O binary out.bin
+```
+
+# Flashing
+
+## dfu-util
+
+Flashing with dfu requires the following procedure.
+1. Disconnect board from USB C.
+2. Short A9 and A10 (You can leave these connected).
+3. Connect USB C to board.
+4. Press BOOT0, NRST.
+5. Release NRST, wait 500ms, Release BOOT0.
+6. Flash with dfu-util with the following command.
+
+```sh
+$ dfu-util -a0 -s 0x08000000  -D out.bin
+```
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+#![no_main]
+#![no_std]
+
+extern crate panic_halt;
+
+use cortex_m as _;
+use cortex_m_rt::entry;
+
+use stm32f4xx_hal as hal;
+
+use hal::prelude::*;
+use hal::stm32;
+
+#[entry]
+fn main() -> ! {
+    let p = stm32::Peripherals::take().unwrap();
+    let cp = cortex_m::peripheral::Peripherals::take().unwrap();
+
+    let gpioc = p.GPIOC.split();
+    let mut led = gpioc.pc13.into_push_pull_output();
+
+    // Set up the system clock. We want to run at 48MHz for this one.
+    let rcc = p.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+
+    // Create a delay abstraction based on SysTick
+    let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
+
+    loop {
+        led.set_low().unwrap();
+        delay.delay_ms(500_u32);
+        led.set_high().unwrap();
+        delay.delay_ms(500_u32);
+    }
+}
